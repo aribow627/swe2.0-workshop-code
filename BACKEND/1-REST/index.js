bff5523d3ea38ebe2b283dab495bc84162d89860
= <90lineng@gmail.com>
2022-09-08 14:21:01 -0400

    rest starter code added

---
+++ BACKEND/1-REST/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const { Dog } = require('./db');
+const { sequelize } = require('./db/db');
+const { Op } = require('sequelize');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get('/dogs/', async (req, res, next) => {
+  //DEMO HERE!!!!!!
+});
+
+//YOU TRY//
+
+
+
+const { PORT = 4000 } = process.env;
+
+app.listen(PORT, () => {
+  sequelize.sync({ force: false });
+  console.log(`Dogs are ready at http://localhost:${PORT}`);
+});
